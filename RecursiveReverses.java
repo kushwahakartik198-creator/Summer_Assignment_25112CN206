dd0c785a664d0af0bdcb5cb721a56f55d4e07aa5
kushwahakartik198 <kushwahakartik198@gmail.com>
2026-06-09 15:33:51 +0530

    day-7,8,9

---
+++ RecursiveReverses.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RecursiveReverses {
+
+    static void reverse(int n) {
+        if (n == 0)
+            return;
+
+        System.out.print(n % 10);
+        reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        reverse(n);
+    }
+}
---
+++ RecursiveSumdigitss.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class RecursiveSumdigitss {
+
+    static int sumDigits(int n) {
+        if (n == 0)
+            return 0;
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(sumDigits(n));
+    }
+}
