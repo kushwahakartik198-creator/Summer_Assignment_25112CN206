a846d1e278e3971710fdf7a72a58f16952f653f7
kushwahakartik198 <kushwahakartik198@gmail.com>
2026-06-24 16:24:56 +0530

    day-13,14,15

---
+++ Q53.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Q53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int key = sc.nextInt();
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Found at index " + index);
+        } else {
+            System.out.println("Not Found");
+        }
+    }
+}
